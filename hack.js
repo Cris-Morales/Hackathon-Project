c47c1108f913462fa1d53f7a3cc3170dadb19b2b
Christian Lyon <christiandoescoding@gmail.com>
2023-08-09 19:17:00 -0500

    Established dummy files

---
+++ hack.js
@@ -0,0 +1,15 @@
+//timer for pomaduro technique
+    //hidden but accessible// adjustable depending on needs
+    //pomodoro function: 25 x 5; 4 times; then 15-30 min break
+        //if clicked, give option to start timer or customize (automatically set to pomodoro)
+
+//once clicked, timer begins and get request sent for lofi vids
+    //created playlist to play in background
+
+//prototype... yellow coloration of screen for readability
+    //button that once clicked, access html and change background color to yellow coloration
+
+//prototype... flash card creation. flash cards can pop onto screen
+//randomly while studying
+    //button that once clicked, opens text box to add information. At random interval, 
+    //flashcard returns with last word removed "______"
